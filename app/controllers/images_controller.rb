8a5889ff0786650c710500955c7056ad09ee52bc
Helmer Avendano <haavendanov@unal.edu.co>
2017-11-23 10:22:25 +0000

    Upload Files and clamAV test

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,94 @@
+require 'fileutils'
+class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
+
+  # GET /images
+  # GET /images.json
+  def index
+    @images = Image.all
+  end
+
+  # GET /images/1
+  # GET /images/1.json
+  def show
+  end
+
+  # GET /images/new
+  def new
+    @image = Image.new
+  end
+
+  # GET /images/1/edit
+  def edit
+  end
+
+  # POST /images
+  # POST /images.json
+  def create
+    @image = Image.new
+    @image.title = params[:image][:title]
+
+    uploaded_io = params[:image][:image_file]
+    random_name = SecureRandom.hex + ".png"
+    File.open(Rails.root.join('public', 'uploads', random_name), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    scan_result = Clamby.safe?('public/uploads/'+random_name)
+    if scan_result
+      FileUtils.mv('public/uploads/'+random_name, 'public/shared/imagenes/' + random_name)
+      @image.name = 'imagenes/' + random_name
+      @image.quarantine = false
+    else
+      FileUtils.mv('public/uploads/'+random_name, 'public/shared/cuarentena/' + random_name)
+      @image.name = 'cuarentena/' + random_name
+      @image.quarantine = true
+    end
+    
+    @image.save!
+
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json { render :show, status: :created, location: @image }
+      else
+        format.html { render :new }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /images/1
+  # PATCH/PUT /images/1.json
+  def update
+    respond_to do |format|
+      if @image.update(image_params)
+        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.json { render :show, status: :ok, location: @image }
+      else
+        format.html { render :edit }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /images/1
+  # DELETE /images/1.json
+  def destroy
+    @image.destroy
+    respond_to do |format|
+      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_image
+      @image = Image.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def image_params
+      params.require(:image).permit(:name, :title, :quarantine)
+    end
+end
--- db/migrate/20171123085345_create_images.rb
+++ db/migrate/20171123085345_create_images.rb
@@ -1,8 +1,9 @@
 class CreateImages < ActiveRecord::Migration[5.1]
   def change
     create_table :images do |t|
+      t.string :name
       t.string :title
-      t.string :image
+      t.boolean :quarantine
 
       t.timestamps
     end
